41bd6c839f1f232b66562d558fc8d1fc690a1356
Slihfer <49499110+Slihfer@users.noreply.github.com>
2022-09-17 20:45:30 +0200

    set up typescript

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import * as socketio from 'socket.io'
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8080
+
+const sio = new socketio.Server(PORT);
+
+sio.on(
+  'connection',
+  (socket) =>
+  {
+    console.log('Connection on port ' + PORT)
+  }
+)
