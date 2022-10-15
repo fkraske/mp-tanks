4102a33ba09669b74404f0e05bff314398cd7e54
Slihfer <49499110+Slihfer@users.noreply.github.com>
2022-10-16 00:55:59 +0200

    update submodule

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { Chronology } from './shared/framework/chronology/Chronology';
 import { Leap } from './shared/framework/chronology/Leap';
 import { Snapshot } from './shared/framework/chronology/Snapshot';
 import { TimeStamped } from './shared/framework/chronology/TimeStamped';
-import { IOEvents } from './shared/framework/communication/events';
-import { AddLeapMessage, ClientMessage, InputMessage, RootUpdateMessage } from './shared/framework/communication/messages';
+import { Input } from './shared/framework/communication/messages';
+import * as ServerEvents from './shared/framework/communication/server/events';
+import * as IOEvents from './shared/framework/communication/socket.io/events';
 import { Time } from './shared/framework/simulation/Time';
-import { MoveInputMessage, TurnInputMessage } from './shared/game/communication/messages';
+import * as ClientEvents from './shared/game/communication/client/events';
+import { MoveInputMessage, TurnInputMessage } from './shared/game/communication/client/messages';
 import { Game } from './shared/game/state/Game';
 
 const chronology = new Chronology<Game>(
@@ -28,7 +30,7 @@ const server = new Server(
   }
 );
 server.on(
-  IOEvents.Builtin.ESTABLISH_CONNECTION,
+  IOEvents.ESTABLISH_CONNECTION,
   (socket: Socket) => {
     if (playerIDs.length == 0) {
       socket.disconnect(true)
@@ -41,48 +43,55 @@ server.on(
 
     socket.join(room)
 
-    console.info('Connection on port ' + Constants.PORT + '. Assigned id: ' + id)
+    console.info('Connection on port ' + Constants.PORT + '. Assigned ID: ' + id)
 
-    socket.on(
-      IOEvents.CUSTOM,
-      (payload: TimeStamped<ClientMessage>) => {
-        console.info('Received message at: ' + payload.timeStamp)
+    //TODO move the leap creation part to shared
+    //TODO generalize
+    //TODO move to abstract subclass for message registrators
+    function registerMessageType<T extends Input>(
+      socket: Socket,
+      event: string,
+      selector: (message: T) => any[] = (_) => [],
+      handler: (message: T) => Leap<Game>
+    ) {
+      socket.on(
+        event,
+        (message: T) => {
+          console.info('Received message at: ' + Time.current)
 
-        /*
-        const m = payload.value
+          if (message.inputTime === undefined || selector(message).some(m => m === undefined)) {
+            console.warn('Received message is incomplete: ' + JSON.stringify(message))
+            return
+          }
 
-        if (m! instanceof InputMessage)
-          return
-
-        let leap: TimeStamped<Leap<Game>>
-
-        if (m instanceof MoveInputMessage) {
-          leap = new TimeStamped<Leap<Game>>(
-            m.inputTime,
-            g => g.addPlayerMoveInput(id, m.directionState)
+          let leap = new TimeStamped<Leap<Game>>(message.inputTime, handler(message))
+          chronology.addTimeStampedLeap(leap)
+          server.except(room).emit(
+            ServerEvents.LEAP,
+            { leap: leap }
           )
-        } else if (m instanceof TurnInputMessage) {
-          leap = new TimeStamped<Leap<Game>>(
-            m.inputTime,
-            g => g.addPlayerTurnInput(id, m.direction)
-          )
-        } else {
-          return
         }
+      )
+    }
 
-        chronology.addTimeStampedLeap(leap)
-        server.except(room).emit(
-          IOEvents.CUSTOM,
-          new AddLeapMessage(leap)
-        )
-        */
-      }
+    registerMessageType<MoveInputMessage>(
+      socket,
+      ClientEvents.INPUT_MOVE,
+      m => [m.directionState],
+      m => g => g.addPlayerMoveInput(id, m.directionState)
+    )
+
+    registerMessageType<TurnInputMessage>(
+      socket,
+      ClientEvents.INPUT_TURN,
+      m => [m.direction],
+      m => g => g.addPlayerTurnInput(id, m.direction)
     )
 
     socket.on(
-      IOEvents.Builtin.DISCONNECT,
+      IOEvents.DISCONNECT,
       () => {
-        console.info('Player with id ' + id + ' disconnected')
+        console.info('Player with ID ' + id + ' disconnected')
         playerIDs.push(id)
       }
     )
@@ -93,10 +102,8 @@ function updateConnections() {
   Time.update()
 
   server.emit(
-    IOEvents.CUSTOM,
-    new RootUpdateMessage(
-      chronology.get(Time.frame)
-    )
+    ServerEvents.UPDATE_ROOT,
+    { state: chronology.get(Time.frame) }
   )
 }
 
